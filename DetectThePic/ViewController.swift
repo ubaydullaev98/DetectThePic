be03ebf5a76ef36b79bc97967808205d30a1a822
Dilmurod Ubaydullaev <dilmurodubaydullaev22@gmail.com>
2019-12-28 19:31:12 +0500

    implement core ml model

--- DetectThePic/ViewController.swift
+++ DetectThePic/ViewController.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var resnetModel = Resnet50()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        processPicture(image: imageView.image!)
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        
+        return cell
+    }
+    
+    func processPicture(image : UIImage){
+        if let model = try? VNCoreMLModel(for: resnetModel.model){
+            let request = VNCoreMLRequest(model: model) { (request, error) in
+                if let results = request.results as? [VNClassificationObservation]{
+                    print(results)
+                }
+            }
+            if let imageData = image.jpegData(compressionQuality: 1.0){
+                let handler = VNImageRequestHandler(data: imageData, options: [:])
+                try? handler.perform([request])
+            }
+        }
+    }
+    
+    
+    
 
 }
 
