e6a16b3222e96467e96779dbabe9393d825debcf
Dilmurod Ubaydullaev <dilmurodubaydullaev22@gmail.com>
2019-12-28 17:51:54 +0500

    first commit

---
+++ DetectThePic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DetectThePic
+//
+//  Created by Dilmurod Ubaydullaev on 12/28/19.
+//  Copyright © 2019 Dilmurod Ubaydullaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
